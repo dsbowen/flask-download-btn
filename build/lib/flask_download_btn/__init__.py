dfa9b3c609f1afed05783055345b96e80713690c
dsbowen <dsbowen@wharton.upenn.edu>
2019-10-30 16:27:57 -0400

    v0.0.1

---
+++ build/lib/flask_download_btn/__init__.py
@@ -0,0 +1,126 @@
+"""Flask-Download-Btn
+
+Flask-Download-Btn defines a SQLAlchemy Mixin for creating bootstrap download buttons 
+in Flask.
+
+This file defines the download button manager. The manager's functions are:
+1. Register download button classes
+2. Hold default settings for download button models
+3. Register routes used by the download button
+
+The download process has three stages:
+1. Web form handling
+2. File creation
+3. Download
+
+The manager's routes reflect the stages of the download process.
+"""
+
+from flask_download_btn.download_btn_mixin import CreateFileMixin, DownloadBtnMixin, HandleFormMixin
+
+from flask import Blueprint, Response, request, session
+import os
+
+"""Default settings for download button models"""
+DEFAULT_SETTINGS = {
+    'btn_classes': ['btn', 'btn-primary', 'w-100'],
+    'btn_style': {},
+    'btn_template': 'download_btn/button.html',
+    'progress_classes': ['progress-bar'],
+    'progress_style': {
+        'height': '25px',
+        'background-color': '#C8C8C8',
+        'margin-top': '10px',
+        'margin-bottom': '10px',
+        'box-shadow': '0 1px 2px rgba(0, 0, 0, 0.25) inset'
+    },
+    'progress_template': 'download_btn/progress.html',
+    'filenames': [],
+    'use_cache': False,
+    'init_transition_speed': '.5s'
+}
+
+
+class DownloadBtnManager():
+    """Download button manager"""
+    registered_classes = {}
+
+    @classmethod
+    def register(cls, btn_cls):
+        """Update download button class registry"""
+        cls.registered_classes[btn_cls.__name__] = btn_cls
+        return btn_cls
+
+    def __init__(self, app=None, db=None, **kwargs):
+        """Constructor
+
+        The constructor updates default settings for buttons. It also 
+        creates a tmp folder for storing zip files which download buttons 
+        will send to the client.
+        """
+        self.db = db
+        self.default_settings = DEFAULT_SETTINGS
+        self.default_settings.update(kwargs)
+        if not os.path.exists('tmp'):
+            os.mkdir('tmp')
+        if app is not None:
+            self._init_app(app)
+    
+    def init_app(self, app, db=None, **kwargs):
+        self.db = db or self.db
+        self.default_settings.update(kwargs)
+        self._init_app(app)
+
+    def _init_app(self, app):
+        self.app = app
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['download_btn_manager'] = self
+        bp = Blueprint('download_btn', __name__, template_folder='templates')
+
+        @bp.route('/download-btn/form/<id>/<btn_cls>', methods=['POST'])
+        def handle_form(id, btn_cls):
+            """Web form handling"""
+            btn = self.get_btn(id, btn_cls)
+            if btn.cached():
+                return ''
+            btn._handle_form(request.form)
+            self.db.session.commit()
+            return ''
+
+        @bp.route('/download-btn/create_files/<id>/<btn_cls>')
+        def create_files(id, btn_cls):
+            """File creation"""
+            btn = self.get_btn(id, btn_cls)
+            if btn.cached():
+                return Response(
+                    btn._download_ready(), mimetype='text/event-stream'
+                )
+            zipf_name = btn._gen_zipf_name()
+            self.db.session.commit()
+            return Response(
+                btn._create_files(app, zipf_name),
+                mimetype='text/event-stream'
+            )
+
+        @bp.route('/download-btn/download/<id>/<btn_cls>')
+        def download(id, btn_cls):
+            """Download files"""
+            btn = self.get_btn(id, btn_cls)
+            if btn.cached():
+                return btn._get_response()
+            response = btn._download()
+            self.db.session.commit()
+            return response
+
+        app.register_blueprint(bp)
+
+    def get_btn(self, id, btn_cls):
+        """Return a download button given its id and class name
+        
+        Authentication is required to access a button for CSRF prevention.
+        """
+        btn = self.registered_classes[btn_cls].query.get(id)
+        if session[btn._csrf_key] == request.args.get('csrf_token'):
+            return btn
+        raise ValueError('CSRF attempt detected and blocked')
---
+++ build/lib/flask_download_btn/download_btn_mixin.py
@@ -0,0 +1,431 @@
+"""Download button mixin
+
+This file defines a download button mixin, as well as mixins for form handling functions and file creation functions.
+
+The download button is responsible for:
+1. Rendering a download button, progress bar, and script
+2. Cache and session management
+3. Handling the download process on click:
+    3.1 Web form handling
+    3.2 File creation
+    3.3 Download
+"""
+
+from datetime import datetime
+from flask import Markup, current_app, render_template, send_file, session
+from random import choice
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text, PickleType, inspect
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.ext.orderinglist import ordering_list
+from sqlalchemy.orm import relationship
+from sqlalchemy_function import FunctionBase
+from sqlalchemy_function import FunctionMixin as FunctionMixinBase
+from sqlalchemy_mutable import MutableListType, MutableDictType
+import itertools
+import json
+import os
+import string
+import zipfile
+
+
+class DownloadBtnMixin(FunctionBase):
+    """Download button mixin"""
+
+    """Columns and relationships"""
+    _zipf_sfxs = Column(MutableListType)
+    btn_classes = Column(MutableListType)
+    btn_style = Column(MutableDictType)
+    btn_template = Column(String)
+    text = Column(Text)
+    progress_classes = Column(MutableListType)
+    progress_style = Column(MutableDictType)
+    progress_template = Column(String)
+    use_cache = Column(Boolean)
+    form_id = Column(String)
+    init_transition_speed = Column(String)
+    filenames = Column(MutableListType)
+    attachment_filename = Column(String)
+    download_msg = Column(Text)
+
+    @declared_attr
+    def handle_form_functions(self):
+        return relationship(
+            'HandleForm',
+            backref='btn',
+            order_by='HandleForm.index',
+            collection_class=ordering_list('index')
+        )
+    
+    @declared_attr
+    def create_file_functions(self):
+        return relationship(
+            'CreateFile',
+            backref='btn',
+            order_by='CreateFile.index',
+            collection_class=ordering_list('index')
+        )
+
+    """Identities"""
+    @property
+    def _id(self):
+        id = inspect(self).identity
+        return id[0] if id else None
+
+    @property
+    def model_id(self):
+        return type(self).__name__+'-'+str(self._id)
+
+    @property
+    def btn_id(self):
+        """Html id for the button <div> tag"""
+        return self.model_id+'-btn'
+
+    @property
+    def progress_id(self):
+        """Html id for the progress bar container""" 
+        return self.model_id+'-progress'
+
+    @property
+    def progress_bar_id(self):
+        """Html id for the progress bar"""
+        return self.model_id+'-progress-bar'
+    
+    @property
+    def progress_text_id(self):
+        """Html id for the progress bar text"""
+        return self.model_id+'-progress-text'
+    
+    """Session keys"""
+    @property
+    def _csrf_key(self):
+        """Key for the CSRF token"""
+        return self.model_id+'csrf'
+
+    @property
+    def _response_cached_key(self):
+        """Key for response caching"""
+        return self.model_id+'cache'
+
+    @property
+    def _zipf_name_key(self):
+        """Key for zip file name"""
+        return self.model_id+'zipf-name'
+
+    @property
+    def _zipf_sfx_key(self):
+        """Key for zip file suffix"""
+        return self.model_id+'zipf-sfx'
+    
+    @property
+    def _filename_key(self):
+        """Key for download file name"""
+        return self.model_id+'filename'
+
+    @property
+    def _attachment_filename_key(self):
+        """Key for attachment file name"""
+        return self.model_id+'attachment_filename'
+
+    """Html properties"""
+    @property
+    def _btn_classes(self):
+        """Button <div> classes in html format"""
+        return ' '.join(self.btn_classes)
+
+    @property
+    def _btn_style(self):
+        return self._style(self.btn_style)
+    
+    @property
+    def _text(self):
+        """Button text"""
+        return self.text or ''
+    
+    @property
+    def _progress_classes(self):
+        """Progress bar <div> classes in html format"""
+        return ' '.join(self.progress_classes)
+
+    @property
+    def _progress_style(self):
+        return self._style(self.progress_style)
+
+    def _style(self, style):
+        return ' '.join([key+': '+val+';' for key, val in style.items()])
+
+    @property
+    def _form(self):
+        """Web form selector"""
+        return 'form' if self.form_id is None else '#'+self.form_id
+
+    def __init__(self, **kwargs):
+        """Constructor
+
+        1. Set function relationships for form handling and file creation 
+        function models.
+        2. Set a blank zip file suffix dictionary
+        3. Set attributes
+
+        Manager defaults are overridden by keyword arguments.
+        """
+        self._set_function_relationships()
+        self._zipf_sfxs = []
+        manager = current_app.extensions['download_btn_manager']
+        self.__dict__.update(manager.default_settings)
+        self.__dict__.update(kwargs)
+
+    """Render download button, progress bar, and script"""
+    def render_btn(self):
+        """Render the download button"""
+        return Markup(render_template(self.btn_template, btn=self))
+
+    def render_progress(self):
+        """Render the progress bar
+        
+        The progress bar is rendered inside a container. The contents of the 
+        container may be updated with progress reports during file creation.
+        """
+        container = '<div id="{0}" style="display: none;">{1}</div>'
+        content = render_template(
+            self.progress_template, btn=self, pct_complete=0, text=''
+        )
+        return Markup(container.format(self.progress_id, content))
+
+    def script(self):
+        """Render the download button script
+        
+        The script will call routes for form handling, file creation, and 
+        download. Authentication for these routes requires a CSRF token. 
+        This method creates a unique token and stores it in the session.
+        """
+        chars = string.ascii_letters + string.digits
+        csrf_token = ''.join([choice(chars) for i in range(90)])
+        session[self._csrf_key] = csrf_token
+        btn_kwargs = {
+            'id': self._id,
+            'btn_cls': type(self).__name__,
+            'csrf_token': csrf_token
+        }
+        return Markup(render_template(
+            'download_btn/script.html', btn=self, btn_kwargs=btn_kwargs
+        ))
+    
+    def reset(self, stage=None, pct_complete=None):
+        """Reset the progress bar
+
+        This method resets the progress bar by replacing its html. You will 
+        typically want to reset the progress bar at the start of a new stage.
+
+        `reset` executes faster than `report`. Therefore, you may also want 
+        to use `reset` instead of `report` when sending rapid progress 
+        updates (~10ms or faster).
+        """
+        text = self._get_progress_text(stage, pct_complete)
+        html = render_template(
+            self.progress_template, 
+            btn=self, pct_complete=pct_complete, text=text
+        )
+        data = json.dumps({'html': html})
+        return 'event: reset\ndata: {}\n\n'.format(data)
+
+    def report(self, stage=None, pct_complete=None):
+        """Send a progress report
+        
+        This method reports file creation progress with a server sent event. 
+        The download button script will update the progress bar accordingly.
+        """
+        text = self._get_progress_text(stage, pct_complete)
+        data = json.dumps({'text': text, 'pct_complete': pct_complete})
+        return 'event: progress_report\ndata: {}\n\n'.format(data)
+
+    def _get_progress_text(self, stage=None, pct_complete=None):
+        """Get progress bar text"""
+        if pct_complete is None:
+            pct_complete, pct_complete_txt = 0, ''
+            stage = stage or ''
+        else:
+            pct_complete_txt = '{:.0f}%'.format(pct_complete)
+            stage = stage+': ' if stage is not None else ''
+        return stage + pct_complete_txt
+    
+    def transition_speed(self, speed):
+        data = json.dumps({'speed': speed})
+        return 'event: transition_speed\ndata: {}\n\n'.format(data)
+
+    """2. Cache and session management"""
+    def cached(self):
+        """Indicate the response is cached"""
+        return (
+            self.use_cache and self._response_cached_key in session
+            and session[self._response_cached_key]
+        )
+    
+    def clear_session(self):
+        """Clear session and delete associated zip file"""
+        name = session[self._zipf_name_key]
+        sfx = session[self._zipf_sfx_key]
+        try:
+            os.remove('tmp/'+name)
+        except:
+            pass
+        self._zipf_sfxs[sfx] = False
+        session.pop(self._zipf_name_key, None)
+        session.pop(self._zipf_sfx_key, None)
+
+    def clear_csrf(self):
+        """Clear CSRF token from session"""
+        session.pop(self._csrf_key, None)
+
+    """3. Web form handling"""
+    """3.1 Web form handling"""
+    def _handle_form(self, response):
+        """Execute handle form functions with form response."""
+        [
+            f.func(f.parent, response, *f.args, **f.kwargs) 
+            for f in self.handle_form_functions
+        ]
+    
+    """3.2 File creation"""
+    def _gen_zipf_name(self):
+        """Generate a unique zip file name for the session
+        
+        This method generates a unique zip file name for the session. The 
+        zip file name will be used to store zipped files. It will not be 
+        necessary to zip files if the download button specifies only one 
+        downloaded file.
+        """
+        sfx = self._gen_zipf_sfx()
+        name = '{0}-{1}.zip'.format(self.model_id, sfx)
+        session[self._zipf_name_key] = name
+        session[self._zipf_sfx_key] = sfx
+        return name
+
+    def _gen_zipf_sfx(self):
+        """Generate a unique zipfile suffix for the session
+        
+        Iterate through the zipfile suffixes until an inactive suffix is 
+        found. Use this suffix for the zipfile name and mark it as active.
+
+        If all suffixes in the zipfile suffix list are active, extend the 
+        list.
+        """
+        for i, active in enumerate(self._zipf_sfxs):
+            if not active:
+                self._zipf_sfxs[i] = True
+                return i
+        self._zipf_sfxs.append(True)
+        return len(self._zipf_sfxs) - 1
+
+    def _create_files(self, app, zipf_name):
+        """Create files for download
+
+        This method performs three tasks:
+        1. Execute the CreateFiles functions
+        2. Zip files if necessary
+        3. Send a 'download_ready' message
+
+        Progress reports are yielded as server sent events.
+        """
+        app.app_context().push()
+        db = app.extensions['download_btn_manager'].db
+        db.session.add(self)
+        gen = itertools.chain(
+            *[f() for f in self.create_file_functions],
+            self._zip_files(zipf_name), 
+            self._download_ready()
+        )
+        sse_prev = sse_curr = datetime.now()
+        for exp in gen:
+            sse_prev, sse_curr = sse_curr, datetime.now()
+            delta = (sse_curr - sse_prev).total_seconds()
+            delta = 0 if delta < .02 else delta
+            yield self.transition_speed(str(delta)+'s')
+            yield exp
+
+    def _zip_files(self, zipf_name):
+        """Zip files
+        
+        Yield a progress report every 10 files.
+        """
+        num_files = len(self.filenames)
+        if num_files <= 1:
+            return
+        stage = 'Zipping Files'
+        yield self.reset(stage, 0)
+        zipf = zipfile.ZipFile('tmp/'+zipf_name, 'w', zipfile.ZIP_DEFLATED)
+        for i, filename in enumerate(self.filenames):
+            if i % 10 == 0:
+                yield self.report(stage, 100.0*i/num_files)
+            zipf.write(filename)
+        yield self.report(stage, 100)
+        zipf.close()
+
+    def _download_ready(self):
+        """Send a download ready message"""
+        text = self._get_progress_text(self.download_msg)
+        data = json.dumps({'text': text, 'pct_complete': 100})
+        yield 'event: download_ready\ndata: {}\n\n'.format(data)
+
+    """3.3 Download"""
+    def _download(self):
+        """Download file
+        
+        Download depends on the number of files to be downloaded:
+        0: Return empty response
+        1: Return the file as an attachment
+        2+: Return the zip archive
+        """
+        num_files = len(self.filenames)
+        if num_files == 0:
+            return ('', 204)
+        zipf_path = 'tmp/'+session[self._zipf_name_key]
+        filename = self.filenames[0] if num_files == 1 else zipf_path
+        if num_files == 1:
+            attachment_filename = self.attachment_filename or filename
+        else:
+            attachment_filename = self.attachment_filename or 'download.zip'
+        session[self._filename_key] = filename
+        session[self._attachment_filename_key] = attachment_filename
+        return self._get_response()
+
+    def _get_response(self):
+        """Get server response (i.e. attachment)"""
+        filename = session[self._filename_key]
+        attachment_filename = session[self._attachment_filename_key]
+        response = send_file(
+            filename, as_attachment=True, 
+            attachment_filename=attachment_filename
+        )
+        self._handle_cache(response)
+        return response
+    
+    def _handle_cache(self, response):
+        """Handle caching"""
+        if not self.use_cache:
+            response.headers['use_cache-Control'] = (
+                'no-use_cache, no-store, must-revalidate'
+            )
+            response.headers['Pragma'] = 'no-use_cache'
+            response.headers['Expires'] = '0'
+            self.clear_session()
+        else:
+            session[self._response_cached_key] = True
+
+
+"""HandleForm and CreateFile Mixins"""
+class FunctionMixin(FunctionMixinBase):
+    @property
+    def parent(self):
+        return self.btn
+
+    @parent.setter
+    def parent(self, value):
+        self.btn = value
+
+
+class HandleFormMixin(FunctionMixin):
+    pass
+
+
+class CreateFileMixin(FunctionMixin):
+    pass
